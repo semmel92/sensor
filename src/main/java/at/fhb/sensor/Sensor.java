d734c9b9ae7131fcca307a837db16ec36927020f
semmel92 <114685286+semmel92@users.noreply.github.com>
2023-09-28 11:09:38 +0200

    Update Sensor.java

--- src/main/java/at/fhb/sensor/Sensor.java
+++ src/main/java/at/fhb/sensor/Sensor.java
@@ -1,13 +1,14 @@
 package com.example.accessingdatarest;
 
+/**
+*
+*    Scheiß Javadoc
+*
+*/
 import jakarta.persistence.Entity;
-// Muss importiert werden, um die Klasse als JPA-Entity zu markieren.
 import jakarta.persistence.Id;
-// Bei Erstellung einer Entity Klasse muss immer ein Feld mit der @Id Annotation versehen werden. Dieses Feld repräsentiert den Primärschlüssel.
 import jakarta.persistence.GeneratedValue;
-// Wird zusammen mit Id importiert und gibt an, dass der Wert des Primärschlüssels automatisch generiert wird, wenn ein Objekt in der Datenbank gespeichert wird.
 import jakarta.persistence.GenerationType;
-// Wird in Verbindung mit GeneratedValue importiert und wird verwendet, um die Strategie für die Generierung der Werte des Primärschlüssels zu spezifizieren. Optional
 
 import java.time.LocalDateTime;
 
